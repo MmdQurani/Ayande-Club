fea20429fe43949e901567bc943c15b11938372a
78 mmd 69 <mmdqurani@gmail.com>
2025-08-21 12:36:56 +0330

    Finishing the Sidebar component and MainLayout file

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,27 +4,24 @@ import logo from '../../assets/images/logo/logo.png'
 import SidebarMenuItem from '../SidebarMenuItem/SidebarMenuItem'
 
 import callCalling from '../../assets/icons/call_calling_sidebar.png'
-
 import { main_menu } from '../../config/menuConfig/menuConfig'
 import { media } from '../../config/mediaConfig/mediaConfig'
 
-function Sidebar() {
+function Sidebar({ isOpen, onClose }) {
   const navigate = useNavigate()
 
   return (
-    <div className="sidebar sticky top-10 h-[90vh] min-h-0 min-w-[267px] hidden lg:flex flex-col bg-white rounded-lg border border-neutral-200 p-4">
-
-      <div className="sidebar_logo mb-6">
-        <img
-          onClick={() => navigate('/dashboard')}
-          className="h-8 w-auto object-contain cursor-pointer"
-          src={logo}
-          alt="Logo"
-        />
+    <div
+      className={` sidebar z-50 h-full lg:h-[90vh] min-h-0 min-w-[267px] flex flex-col bg-white rounded-lg border border-neutral-200 p-4 transition-transform duration-300 fixed top-0 lg:sticky lg:top-10 right-0 ${isOpen ? "translate-x-0" : "translate-x-full lg:translate-x-0"}`}>
+      <div className="sidebar_logo mb-6 flex justify-between items-center">
+        <img onClick={() => navigate('/dashboard')} className="h-8 w-auto object-contain cursor-pointer" src={logo} alt="Logo" />
+        {/* دکمه بستن فقط برای موبایل */}
+        <button onClick={onClose} className="lg:hidden p-2 rounded-md hover:bg-gray-100">
+          ✕
+        </button>
       </div>
 
       <div className="sidebar_menu flex flex-col justify-between flex-1 min-h-0">
-
         <ul className="overflow-y-auto flex-1 min-h-0 mb-24 [&::-webkit-scrollbar]:hidden [scrollbar-width:none] [-ms-overflow-style:none] space-y-2">
           {main_menu.map(item => (
             <li key={item.title}>
@@ -36,7 +33,6 @@ function Sidebar() {
         </ul>
 
         <div className="sidebar_footer">
-
           <div className="mobile_contact0 py-6 border-t border-neutral-200 flex justify-between text-xs">
             <div className="flex items-center space-x-1.5">
               <img className="size-5 object-contain" src={callCalling} alt="تماس" />
@@ -53,7 +49,6 @@ function Sidebar() {
               </Link>
             ))}
           </div>
-
         </div>
       </div>
     </div>
--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,28 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainHeader from '../Components/MainHeader/MainHeader'
 import { useNavigate } from 'react-router-dom'
 import Sidebar from '../Components/Sidebar/Sidebar'
 
-function MainLayout({ children }) {
+import logo from '../assets/images/logo/logo.png'
 
+function MainLayout({ children }) {
   const navigate = useNavigate()
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
   return (
-    <>
-      <div className='MainLayout w-full min-h-screen bg-gray-100/60 flex justify-center' dir='rtl'>
+    <div className='MainLayout w-full min-h-screen bg-gray-100/60 flex lg:flex-row flex-col justify-center relative' dir='rtl'>
 
-        <div className='lg:container w-full px-2 flex flex-row justify-center my-10 space-x-4'>
-          <Sidebar />
+      <div className="lg:hidden w-full main_header_mobile bg-white rounded-b-xl shadow-md flex flex-row justify-between p-4">
+        <button
+          onClick={() => setIsSidebarOpen(true)}
+          className="px-4 py-2 stroke-neutral-800 cursor-pointer rounded-md hover:shadow-md transition duration-200"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+            <path d="M4 6H20M4 12H20M4 18H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </button>
 
-          <main className='main flex-1 min-w-0'>
-            <MainHeader />
+        <img className='h-8 w-auto object-contain' src={logo} alt="logo" />
 
-            {children}
-          </main>
-        </div>
+        <button
+          onClick={() => setIsSidebarOpen(true)}
+          className="px-4 py-2 stroke-neutral-800 cursor-pointer rounded-md hover:shadow-md transition duration-200"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none"><path d="M12.1601 10.87C12.0601 10.86 11.9401 10.86 11.8301 10.87C9.45006 10.79 7.56006 8.84 7.56006 6.44C7.56006 3.99 9.54006 2 12.0001 2C14.4501 2 16.4401 3.99 16.4401 6.44C16.4301 8.84 14.5401 10.79 12.1601 10.87Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7.15997 14.56C4.73997 16.18 4.73997 18.82 7.15997 20.43C9.90997 22.27 14.42 22.27 17.17 20.43C19.59 18.81 19.59 16.17 17.17 14.56C14.43 12.73 9.91997 12.73 7.15997 14.56Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+        </button>
 
       </div>
-    </>
+
+      <div className='lg:container w-full px-2 flex flex-row justify-center my-0 lg:my-10 space-x-4'>
+
+        {/* سایدبار */}
+        <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+
+        {isSidebarOpen && (
+          <div
+            onClick={() => setIsSidebarOpen(false)}
+            className="fixed inset-0 m-0 bg-black/40 backdrop-blur-sm z-40 lg:hidden"
+          />
+        )}
+
+        <main className='main flex-1 min-w-0 relative z-0'>
+          <MainHeader />
+          {children}
+        </main>
+      </div>
+    </div>
   )
 }
 
